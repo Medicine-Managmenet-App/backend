517eb36ec66a78fed9403160e7fd432434dad8de
Aleksandra Kemona <aleksandra.kemona@gmail.com>
2023-06-12 12:18:39 +0200

    fix: added missing class body to FamilyMemberController

--- src/main/java/pl/zaprogramujzycie/mma/controllers/FamilyMemberController.java
+++ src/main/java/pl/zaprogramujzycie/mma/controllers/FamilyMemberController.java
@@ -1,4 +1,97 @@
 package pl.zaprogramujzycie.mma.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import pl.zaprogramujzycie.mma.DTO.FamilyMemberDTO;
+
+import java.util.List;
+
 public class FamilyMemberController {
+
+    private FamilyMemberService service;
+
+    FamilyMemberDTO dto;
+
+
+    @Operation(
+            operationId = "getFamilyMembers",
+            description = "returns all registered family members",
+            tags = "FamilyMembers"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "FamilyMembers found", ref = "#/components/schemas/FamilyMember"),
+            @ApiResponse(responseCode = "500", description = "internal server error")
+    })
+    @GetMapping
+    public ResponseEntity<List<FamilyMemberDTO>> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Operation(
+            operationId = "createFamilyMember",
+            description = "create new family member",
+            tags = "FamilyMembers"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "FamilyMember created successfully"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "internal server error")
+    })
+    @PostMapping
+    ResponseEntity<FamilyMemberDTO> createFamilyMember(@RequestBody FamilyMemberDTO newFamilyMemberRequest, UriComponentsBuilder ucb) {
+        return null;
+    }
+
+    @Operation(
+            operationId = "getFamilyMemberById",
+            description = "get family member by Id",
+            tags = "FamilyMember"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "FamilyMember found", ref = "#/components/schemas/FamilyMember"),
+            @ApiResponse(responseCode = "404", description = "FamilyMember not found"),
+            @ApiResponse(responseCode = "500", description = "internal server error")
+    })
+    @GetMapping("familymembers/{requestedId}")
+    public ResponseEntity<FamilyMemberDTO> findById(@PathVariable Long requestedId) {
+        return null;
+    }
+
+
+    @Operation(
+            operationId = "UpdateFamilyMember",
+            description = "update family member",
+            tags = "FamilyMember"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "FamilyMember updated successfully", ref = "#/components/schemas/FamilyMember"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "404", description = "FamilyMember not found"),
+            @ApiResponse(responseCode = "500", description = "internal server error")
+    })
+    @PatchMapping("familymembers/{requestedId}")
+    private ResponseEntity<FamilyMemberDTO> updateFamilyMember(@PathVariable Long id, @RequestBody FamilyMemberDTO familyMemberDto) {
+        return null;
+    }
+
+    @Operation(
+            operationId = "Delete family member by Id",
+            description = "deleteFamilyMember",
+            tags = "FamilyMember"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "FamilyMember deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "FamilyMember not found"),
+            @ApiResponse(responseCode = "500", description = "internal server error")
+    })
+
+    @DeleteMapping("/{requestedId}")
+    private ResponseEntity<String> deleteFamilyMember(@PathVariable Long id) {
+        return null;
+    }
 }
