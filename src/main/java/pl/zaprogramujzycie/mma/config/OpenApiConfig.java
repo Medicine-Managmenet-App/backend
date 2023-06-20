27611da28ef184918d35aeb3121ea4b45d7e452d
karol.wolny <revodev500@gmail.com>
2023-06-20 19:21:19 +0200

    refactor: optimize imports

--- src/main/java/pl/zaprogramujzycie/mma/config/OpenApiConfig.java
+++ src/main/java/pl/zaprogramujzycie/mma/config/OpenApiConfig.java
@@ -1,13 +1,11 @@
 package pl.zaprogramujzycie.mma.config;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
