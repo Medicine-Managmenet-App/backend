e45ec3511e4547f7dd710c74ca76debb4328d530
Aleksandra Kemona <aleksandra.kemona@gmail.com>
2023-06-26 20:01:23 +0200

    fix: fixing migrations

---
+++ target/classes/db/migration/V6__Add_prescriptions.sql
@@ -0,0 +1,20 @@
+insert into prescriptions(id, family_id) values (100, 100);
+insert into prescriptions(id, family_id) values (101, 100);
+insert into prescriptions(id, family_id) values (102, 100);
+insert into prescriptions(id, family_id) values (103, 101);
+
+insert into prescribed_medicines(id, medicine_id, dosage, number_of_doses, dosage_interval, prescription_id) values (100, 100, 1.00, 3, 24, 100);
+insert into prescribed_medicines(id, medicine_id, dosage, number_of_doses, dosage_interval, prescription_id) values (101, 101, 1.00, 3, 24, 100);
+insert into prescribed_medicines(id, medicine_id, dosage, number_of_doses, dosage_interval, prescription_id) values (102, 102, 1.00, 3, 24, 101);
+insert into prescribed_medicines(id, medicine_id, dosage, number_of_doses, dosage_interval, prescription_id) values (103, 103, 1.00, 3, 24, 103);
+
+insert into prescribed_medicine_administration_times (prescribed_medicine_id, administration_times) values (100, '8:00:00');
+insert into prescribed_medicine_administration_times (prescribed_medicine_id, administration_times) values (100, '16:00:00');
+insert into prescribed_medicine_administration_times (prescribed_medicine_id, administration_times) values (100, '00:00:00');
+
+insert into prescribed_medicine_administration_times (prescribed_medicine_id, administration_times) values (101, '9:00:00');
+insert into prescribed_medicine_administration_times (prescribed_medicine_id, administration_times) values (101, '17:00:00');
+insert into prescribed_medicine_administration_times (prescribed_medicine_id, administration_times) values (101, '01:00:00');
+
+
+
---
+++ target/classes/db/migration/V9__Add_users.sql
@@ -0,0 +1,5 @@
+insert into users (id, login, password, family_id) values (100, 'login', 'password', 100);
+insert into users (id, login, password, family_id) values (101, 'login1', 'password1', 100);
+insert into users (id, login, password, family_id) values (102, 'login2', 'password2', 100);
+insert into users (id, login, password, family_id) values (103, 'login3', 'password3', 101);
+
