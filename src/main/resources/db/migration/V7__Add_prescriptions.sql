1023d5477700e332ccd038f23e7ea2fb86a76288
Aleksandra Kemona <aleksandra.kemona@gmail.com>
2023-06-28 18:54:36 +0200

    fix: updating flyway migrations

--- src/main/resources/db/migration/V7__Add_prescriptions.sql
+++ src/main/resources/db/migration/V7__Add_prescriptions.sql
@@ -1,7 +1,7 @@
-insert into prescriptions(id, family_id) values (100, 100);
-insert into prescriptions(id, family_id) values (101, 100);
-insert into prescriptions(id, family_id) values (102, 100);
-insert into prescriptions(id, family_id) values (103, 101);
+insert into prescriptions(id, family_member_id) values (100, 100);
+insert into prescriptions(id, family_member_id) values (101, 101);
+insert into prescriptions(id, family_member_id) values (102, 102);
+insert into prescriptions(id, family_member_id) values (103, 103);
 
 insert into prescribed_medicines(id, medicine_id, dosage, number_of_doses, dosage_interval, prescription_id) values (100, 100, 1.00, 3, 24, 100);
 insert into prescribed_medicines(id, medicine_id, dosage, number_of_doses, dosage_interval, prescription_id) values (101, 101, 1.00, 3, 24, 100);
