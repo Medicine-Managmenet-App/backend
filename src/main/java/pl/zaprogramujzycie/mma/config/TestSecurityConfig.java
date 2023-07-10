007ce20c0e744414fc90579e0107d95a5afc2bac
Aleksandra Kemona <aleksandra.kemona@gmail.com>
2023-07-10 23:27:29 +0200

    feature: adding heroku system.properties

--- src/main/java/pl/zaprogramujzycie/mma/config/TestSecurityConfig.java
+++ src/main/java/pl/zaprogramujzycie/mma/config/TestSecurityConfig.java
@@ -30,7 +30,7 @@ public class TestSecurityConfig {
     }
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("/users", "/v3/api-docs", "/v2/api-docs",
+        return (web) -> web.ignoring().antMatchers("/users", "/v3/api-docs/**", "/v2/api-docs",
                 "/configuration/ui", "/swagger-ui.html", "swagger-resources", "/swagger-resources/**", "/configuration/security",
                 "/swagger-ui/**", "swagger-ui/index.html");
     }
