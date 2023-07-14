a9aaef88d5d3ce9bf62497c8bd15c21bcdc583fb
Aleksandra Kemona <aleksandra.kemona@gmail.com>
2023-07-14 21:30:37 +0200

    fix: fixing cors configuration

--- src/main/java/pl/zaprogramujzycie/mma/config/ProdSecurityConfig.java
+++ src/main/java/pl/zaprogramujzycie/mma/config/ProdSecurityConfig.java
@@ -58,7 +58,7 @@ public class ProdSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("/*"));
         // configuration.setAllowCredentials(true);
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "HEAD", "DELETE", "OPTIONS"));
         // configuration.setAllowedHeaders(Arrays.asList("*"));
