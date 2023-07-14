efe67566b0bf1679601e4cbe90405b1159d7106c
Aleksandra Kemona <aleksandra.kemona@gmail.com>
2023-07-14 22:29:24 +0200

    fix: fixing cors configuration

--- src/main/java/pl/zaprogramujzycie/mma/controllers/UserController.java
+++ src/main/java/pl/zaprogramujzycie/mma/controllers/UserController.java
@@ -48,6 +48,7 @@ public class UserController {
             @ApiResponse(responseCode = "400", description = "Bad request"),
             @ApiResponse(responseCode = "500", description = "Internal server error")
     })
+    @CrossOrigin(allowedHeaders = "*")
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@RequestBody final UserRequest newUserRequest) {
         System.out.println("-----controller------");
