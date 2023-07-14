0cd81f1307687a9f538a5c9914cb5766eb1eb410
Aleksandra Kemona <aleksandra.kemona@gmail.com>
2023-07-14 13:57:38 +0200

    fix: cleaning after merge of working app and master

--- src/main/java/pl/zaprogramujzycie/mma/controllers/UserController.java
+++ src/main/java/pl/zaprogramujzycie/mma/controllers/UserController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ import java.security.Principal;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin
 public class UserController {
   
     final UserService service;
